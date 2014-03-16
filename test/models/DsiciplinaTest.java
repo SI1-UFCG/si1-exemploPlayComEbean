a62a9813d8330aa9d40b36f7d700a9a6c954d7a3
Nazareno <nazareno@computacao.ufcg.edu.br>
2014-03-16 13:51:27 -0300

    merged com o repositorio criado no github

--- test/models/DsiciplinaTest.java
+++ test/models/DsiciplinaTest.java
@@ -4,12 +4,16 @@ import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.start;
-import models.Disciplina;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DsiciplinaTest {
+
+	Disciplina si1 = new Disciplina("SI1", "Sistemas de Informação 1", 4);
 
 	@Before
 	public void setUp() throws Exception {
@@ -17,14 +21,29 @@ public class DsiciplinaTest {
 	}
 
 	@Test
-	public void deveConseguirSalvar() {
-		Disciplina d = new Disciplina("SI1", "Sistemas de Informação 1", 4);
-		d.save();
+	public void deveConseguirSalvarSemPrerequisitos() {
+		si1.save();
 
-		// recupera
 		Disciplina recuperada = Disciplina.find.findUnique();
 
-		assertThat(recuperada).equals(d);
+		assertThat(recuperada).isEqualTo(si1);
 	}
 
+	@Test
+	public void deveConseguirSalvarComPrerequisitos() {
+		si1.save();
+		
+		Disciplina si2 = new Disciplina("SI2", "Sistemas de Informação 2", 4,
+				Arrays.asList(new Disciplina[] { si1 }));
+		Disciplina optativa = new Disciplina("OPT", "TECC: Seiquelá", 4,
+				Arrays.asList(new Disciplina[] { si1 }));
+
+		si2.save();
+		optativa.save();
+
+		List<Disciplina> todas = Disciplina.find.all();
+		assertThat(todas.size()).isEqualTo(3);
+		
+		
+	}
 }
