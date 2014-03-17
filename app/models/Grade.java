0a93f10c6f35095188f79aeab83a0eaf229c55f6
Nazareno <nazareno@computacao.ufcg.edu.br>
2014-03-16 23:31:01 -0300

    Grade com explicações

--- app/models/Grade.java
+++ app/models/Grade.java
@@ -15,6 +15,11 @@ public class Grade extends Model {
 	@Id
 	private Long id;
 
+	/*
+	 * Por causa de como o Ebean funciona, é mais fácil colocar relações
+	 * OneToMany unidirecionais como ManyToMany. Na prática, gasta um pouco mais
+	 * de espaço, mas fica muito mais simples.
+	 */
 	@ManyToMany
 	@JoinTable(name = "Grade_Disciplinas")
 	private List<Disciplina> disciplinas;
